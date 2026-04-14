2f4c4ec3c206f730213376c4245e6336f433daf3
nhatanh-h2 <anh.ngo@h2corporation.jp>
2026-04-14 20:43:13 +0700

    UPDATE: made go's derivation override more robust to nixpkgs changes between revisions

--- packages/go/default.nix
+++ packages/go/default.nix
@@ -10,29 +10,49 @@ let
   version = "1.26.2";
   goBootstrap = buildPackages.callPackage ./bootstrap.nix { };
 in
-go.overrideAttrs (old: {
-  inherit version;
-  src = builtins.fetchurl {
-    url = "https://go.dev/dl/go${version}.src.tar.gz";
-    sha256 = "sha256:0fyldyyrval29zngyp9pdlxfdvq2i1m95cxjdx1yk5ksjjvfp49f";
-  };
-  patches = [
-    (replaceVars ./iana-etc.patch {
-      iana = iana-etc;
-    })
-    # Patch the mimetype database location which is missing on NixOS.
-    # but also allow static binaries built with NixOS to run outside nix
-    (replaceVars ./mailcap.patch {
-      inherit mailcap;
-    })
-    # prepend the nix path to the zoneinfo files but also leave the original value for static binaries
-    # that run outside a nix server
-    (replaceVars ./tzdata.patch {
-      inherit tzdata;
-    })
-    ./remove-tools.patch
-    ./go_no_vendor_checks.patch
-    ./go-env-go_ldso.patch
-  ];
-  GOROOT_BOOTSTRAP = "${goBootstrap}/share/go";
-})
+go.overrideAttrs (
+  old:
+  {
+    inherit version;
+    src = builtins.fetchurl {
+      url = "https://go.dev/dl/go${version}.src.tar.gz";
+      sha256 = "sha256:0fyldyyrval29zngyp9pdlxfdvq2i1m95cxjdx1yk5ksjjvfp49f";
+    };
+    patches = [
+      (replaceVars ./iana-etc.patch {
+        iana = iana-etc;
+      })
+      # Patch the mimetype database location which is missing on NixOS.
+      # but also allow static binaries built with NixOS to run outside nix
+      (replaceVars ./mailcap.patch {
+        inherit mailcap;
+      })
+      # prepend the nix path to the zoneinfo files but also leave the original value for static binaries
+      # that run outside a nix server
+      (replaceVars ./tzdata.patch {
+        inherit tzdata;
+      })
+      ./remove-tools.patch
+      ./go_no_vendor_checks.patch
+      ./go-env-go_ldso.patch
+    ];
+  }
+  # deduplicate overlapping GOROOT_BOOTSTRAP sets
+  // (
+    let
+      # switch on this condition because depending on the nixpkgs revision this would be expected to be set at different place
+      isInEnv = (old.env or { }) ? GOROOT_BOOTSTRAP;
+    in
+    if isInEnv then
+      {
+        env = builtins.removeAttrs (old.env or { }) [ "GOROOT_BOOTSTRAP" ] // {
+          GOROOT_BOOTSTRAP = "${goBootstrap}/share/go";
+        };
+      }
+    else
+      {
+        env = builtins.removeAttrs (old.env or { }) [ "GOROOT_BOOTSTRAP" ];
+        GOROOT_BOOTSTRAP = "${goBootstrap}/share/go";
+      }
+  )
+)
