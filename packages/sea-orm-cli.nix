3cd150ae9bfa1f83988bd3385f79f4139486c3bb
nhatanh-h2 <anh.ngo@h2corporation.jp>
2026-04-16 15:47:10 +0700

    FIXED: make the override for sea-orm-cli more sensible and robust

--- packages/sea-orm-cli.nix
+++ packages/sea-orm-cli.nix
@@ -1,28 +1,20 @@
 {
   sea-orm-cli,
-  rustPlatform,
   fetchCrate,
+  rustPlatform,
 }:
-sea-orm-cli.override (
-  let
-    rp = rustPlatform;
-  in
-  {
-    rustPlatform = rustPlatform // {
-      buildRustPackage =
-        args:
-        rp.buildRustPackage (
-          args
-          // {
-            version = "1.1.19";
-            src = fetchCrate {
-              inherit (args) pname;
-              version = "1.1.19";
-              hash = "sha256-dsise5MDhR4pcD3ZWDUzTG0Q4Fg/VdKw2Q59/g6BabA=";
-            };
-            cargoHash = "sha256-38KIJYwRvVmChGSJwaRRWbb/HPuuTp/qnvXpo3xjRpE=";
-          }
-        );
+sea-orm-cli.overrideAttrs (
+  final: old: {
+    version = "1.1.19";
+    src = fetchCrate {
+      pname = old.pname;
+      version = "1.1.19";
+      hash = "sha256-dsise5MDhR4pcD3ZWDUzTG0Q4Fg/VdKw2Q59/g6BabA=";
+    };
+    cargoDeps = rustPlatform.fetchCargoVendor {
+      name = "${final.pname}-vendor.tar.gz";
+      src = final.src;
+      hash = "sha256-38KIJYwRvVmChGSJwaRRWbb/HPuuTp/qnvXpo3xjRpE=";
     };
   }
 )
