a289b46aac13c84910504b448e33cda58e912552
nhatanh-h2 <anh.ngo@h2corporation.jp>
2026-09-10 03:09:51 +0700

    FIXED: go bootstrap hash for arm64-darwin

--- packages/go/bootstrap.nix
+++ packages/go/bootstrap.nix
@@ -3,7 +3,7 @@ callPackage ./binary.nix {
   version = "1.26.3";
   hashes = {
     # from https://go.dev/dl/?mode=json&include=all
-    darwin-arm64 = "278d580b32e299fe4a9c990fcf2d02acfe538c7e551a6ee18f9c7164573d2c63";
+    darwin-arm64 = "875cf54a15311eee2c99b9dd67c68c4a49351d489ab622bf2cfd28c8f2078d3c";
     linux-amd64 = "2b2cfc7148493da5e73981bffbf3353af381d5f93e789c82c79aff64962eb556";
   };
 }
